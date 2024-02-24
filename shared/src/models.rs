eb1d9bba3451ff607716e3029ba51726c1aadcc0
Henri <h.w.tompodung@gmail.com>
2024-02-24 22:22:24 +0700

    add postgres film repository

--- shared/src/models.rs
+++ shared/src/models.rs
@@ -3,21 +3,25 @@ use serde::{
     Serialize,
 };
 
+#[cfg_attr(feature = "backend", derive(sqlx::FromRow))]
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct Film {
     pub id: uuid::Uuid,
     pub title: String,
     pub director: String,
+    #[sqlx(try_from = "i16")]
     pub year: u16,
     pub poster: String,
     pub created_at: Option<chrono::DateTime<chrono::Utc>>,
     pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
 }
 
+#[cfg_attr(feature = "backend", derive(sqlx::FromRow))]
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct CreateFilm {
     pub title: String,
     pub director: String,
+    #[sqlx(try_from = "i16")]
     pub year: u16,
     pub poster: String,
 }

c375ea18ffbc75d649eff5bec31abdc1266d7d3d
Henri <h.w.tompodung@gmail.com>
2024-02-27 10:09:04 +0700

    added Header and Footer components

---
+++ front/src/components/mod.rs
@@ -0,0 +1,5 @@
+mod header;
+mod footer;
+
+pub use footer::Footer;
+pub use header::Header;
--- front/src/main.rs
+++ front/src/main.rs
@@ -1,5 +1,8 @@
 #[allow(non_snake_case)]
 // Import the Dioxus prelude to gain access to the `rsx!` macro and the `Scope` and `Element` types.
+mod components;
+
+use components::{Header, Footer};
 use dioxus::prelude::*;
 
 fn main() {
@@ -11,8 +14,13 @@ fn main() {
 // Define a component that renders a div with the text "Hello, world!"
 fn App(cx: Scope) -> Element {
     cx.render(rsx! {
-        div {
-            "Hello Dioxus!"
+        main {
+            class: "relative z-0 bg-blue-100 w-screen h-auto min-h-screen flex flex-col justify-start items-stretch",
+            Header {}
+            section {
+                class: "md:container md:mx-auto md:py-8 flex-1",
+            }
+            Footer{}
         }
     })
 }
